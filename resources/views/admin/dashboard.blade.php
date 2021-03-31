f3d3154d75d043d18cf26ac46776d5921972edda
wildan <wildan123muhamad@gmail.com>
2021-03-31 10:32:42 +0700

    login

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -311,7 +311,7 @@
 
                         <div class="media">
                             <a href="#">
-                                <img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" src="images/admin.jpg">
+                                <img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" src="public/images/admin.jpg">
                             </a>
                             <div class="media-body">
                                 <h2 class="text-white display-6">Jim Doe</h2>
