660679f3e16eea96353d773acfc1ba887b12b18f
wildan <wildan123muhamad@gmail.com>
2021-04-20 14:08:02 +0700

    task index berhasil

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin','middleware' => 'auth'], function(){
     Route::get('/','Admin\DashboardController@index');
     Route::resource('/kategori', 'Admin\KategoriController');
+    Route::resource('/tugas', 'Admin\TugasController');
 });
