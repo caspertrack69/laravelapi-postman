cbf5de89eff61e2e0c88af9280ab5ceee1d6bcce
DESKTOP-IN7C8M5\ASUS PC <r.tullah767@gmail.com>
2021-09-26 01:03:37 +0700

    nambah api login

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -26,5 +27,35 @@ class AuthController extends Controller
         $user->save;
 
         return response()->json($user, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validateData = $request->validate([
+            'email' => 'email|required | unique:users',
+            'password' => 'required | confirmed',
+        ]);
+
+        $login_detail = request(['email', 'password']);
+
+        if (!Auth::attempt($login_detail)) {
+            return response()->json([
+                'error' => 'login gagal. cek lagi detail login'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('AccessToken');
+        $token = $tokenResult->token;
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_id' => $token->id,
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('register', 'Api\Auth\AuthController@register');
+Route::post('login', 'Api\Auth\AuthController@login');
